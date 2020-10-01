7e3c38207e321eb0429aae639dcce3e8483986cf
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2020-10-01 10:11:25 +0200

    Updated development.sh alias file. Added .gitignore; Ignoring .idea folder.

--- aliases/development.sh
+++ aliases/development.sh
@@ -13,6 +13,8 @@ alias cfresh="rm -rf vendor/ composer.lock && composer i"
 alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
 alias watch="npm run watch"
 
+# alias comp ="COMPOSER_MEMORY_LIMIT=-1 composer"
+
 # Composer
 composer-link () {
     composer config repositories.local '{"type": "path", "url": "'$1'"}' --file composer.json
