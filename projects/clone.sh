50f5fa19317c4a592356b8d6803a655f6cef6878
Wouter van Marrum <wouter@motivo.nl>
2021-01-29 07:54:31 +0100

    Added the zsh-completion plugin

--- projects/clone.sh
+++ projects/clone.sh
@@ -6,6 +6,9 @@ PROJECTS=$HOME/projects
 PERSONAL=$PROJECTS/personal
 WORK=$PROJECTS/work
 
+# ZSH 
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+
 # Personal
 git clone git@github.com:wotta/wotta.git $PERSONAL/wotta
 git clone git@github.com:wotta/illuminate-extender.git $PERSONAL/illuminate-extender
