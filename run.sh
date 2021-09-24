b6dc28aa6cd562319e13cd4eb8e1782d9729100a
Wouter <wouter.van.marrum@protonmail.com>
2021-09-24 22:10:57 +0200

    Removed some setup code from run.sh

--- run.sh
+++ run.sh
@@ -14,19 +14,6 @@ brew update
 brew tap homebrew/bundle
 brew bundle
 
-# Set default MySQL root password and auth type.
-mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'root'; FLUSH PRIVILEGES;" || true
-
-# Install PHP extensions with PECL
-pecl install memcached imagick xdebug
-
-# Install global Composer packages
-/usr/local/bin/composer global require \
-    laravel/installer \
-    laravel/valet \
-    beyondcode/expose \
-    tightenco/takeout
-
 # Install global npm packages
 /usr/local/bin/npm install --global \
 	kmdr # https://kmdr.sh
@@ -52,10 +39,6 @@ ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 ln -s $HOME/.dotfiles/.gitignore_global $HOME/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
 
-# Add ini symlink so we can easily acces it.
-# This can be done as: php -c ./disable-xdebug.ini vendor/bin/phpunit
-ln -s $HOME/.dotfiles/php/ini/disable-xdebug.ini $HOME/disable-xdebug.ini
-
 # Symlink spaceship theme
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme" 
 
@@ -64,9 +47,6 @@ compaudit | xargs chmod g-w,o-w
 # chmod g-w,o-w /usr/local/share/zsh
 # chmod g-w,o-w /usr/local/share/zsh/site-functions
 
-# Symlink the Mackup config file to the home directory
-# ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
-
 # Set macOS preferences
 # We will run this last because this will reload the shell
-source .macos
+# source .macos
