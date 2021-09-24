4728544a2fd2d88e7fcf46e75cc4ae2b2789375e
Wouter <wouter.van.marrum@protonmail.com>
2021-09-24 21:47:55 +0200

    Updated the clone.sh

--- projects/clone.sh
+++ projects/clone.sh
@@ -4,14 +4,9 @@ echo "Cloning repositories..."
 
 PROJECTS=$HOME/projects
 PERSONAL=$PROJECTS/personal
-WORK=$PROJECTS/work
 
 # ZSH 
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 
 # Personal
 git clone git@github.com:wotta/wotta.git $PERSONAL/wotta
-git clone git@github.com:wotta/illuminate-extender.git $PERSONAL/illuminate-extender
-git clone git@github.com:wotta/consolehints.git $PERSONAL/consolehints
-
-# Work
