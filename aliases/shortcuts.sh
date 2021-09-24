cb4cb38d083652ee271f1a34e6d0cf87b8d217ca
Wouter <wouter.van.marrum@protonmail.com>
2021-09-24 22:01:09 +0200

    Updated the shortcuts shell script

--- aliases/shortcuts.sh
+++ aliases/shortcuts.sh
@@ -13,12 +13,7 @@ alias back='cd $OLDPWD'
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 
 alias reloadshell="source $HOME/.zshrc"
-alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
-
-alias localwork="ssh-add -D; ssh-add ~/.ssh/id_rsa"
-alias companywork="ssh-add -D; ssh-add ~/.ssh/motivo_rsa"
 
 # shortcuts to configs
 alias eV='vim ~/.vimrc'
 alias eZ='vim ~/.zshrc'
-alias phpunit="./vendor/bin/phpunit"
