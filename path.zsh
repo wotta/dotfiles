137b5377fb359165876feb794d8304d6b2096a7a
Wouter <wouter.van.marrum@protonmail.com>
2021-09-25 23:03:43 +0200

    Updated paths in path.sh

--- path.zsh
+++ path.zsh
@@ -1,26 +1,12 @@
-# Load Composer tools
-export PATH="$HOME/.composer/vendor/bin:$PATH"
-
-# Load Node global installed binaries
-export PATH="$HOME/.node/bin:$PATH"
-
-# Use project specific binaries before global ones
-export PATH="node_modules/.bin:vendor/bin:$PATH"
-
 # Ruby tools
 #export GEM_HOME="$HOME/.gem/ruby/2.3.0"
 export GEM_HOME="$HOME/.gem/ruby/2.6.0"
 #export GEM_HOME="$HOME/.gem/ruby/2.7.0"
 export PATH="$GEM_HOME/bin:$PATH"
 
-# PHP 7.1 tools
-export PATH="/usr/local/opt/icu4c/bin:$PATH"
-export PATH="/usr/local/opt/icu4c/sbin:$PATH"
-
 # Make sure coreutils are loaded before system commands
 # I've disabled this for now because I only use "ls" which is
 # referenced in my aliases.zsh file directly.
 #export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
 
-export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
 export PATH="/usr/local/sbin:$PATH"
