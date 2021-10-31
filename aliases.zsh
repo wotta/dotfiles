0b8a954642f30232c6a87095da2a37a666482d9b
Wouter <wouter.van.marrum@protonmail.com>
2021-10-31 16:06:03 +0100

    Added new aliases

--- aliases.zsh
+++ aliases.zsh
@@ -4,3 +4,9 @@ source $DOTFILES/aliases/system.sh
 source $DOTFILES/aliases/shortcuts.sh
 source $DOTFILES/aliases/git.sh
 source $DOTFILES/aliases/extra.sh
+
+composer-link() {
+    ./vendor/bin/sail composer config repositories.local '{"type": "path", "url": "'$1'"}' --file composer.json
+}
+
+alias sail='[ -f sail ] && bash sail || bash vendor/bin/sail'
