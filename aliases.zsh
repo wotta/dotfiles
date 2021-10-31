dca17259ccd7b3c51a9befbbfbd19bc3fdfa6e7f
Wouter <wouter.van.marrum@protonmail.com>
2021-10-31 16:07:11 +0100

    Removed extra.sh

--- aliases.zsh
+++ aliases.zsh
@@ -3,7 +3,6 @@
 source $DOTFILES/aliases/system.sh
 source $DOTFILES/aliases/shortcuts.sh
 source $DOTFILES/aliases/git.sh
-source $DOTFILES/aliases/extra.sh
 
 composer-link() {
     ./vendor/bin/sail composer config repositories.local '{"type": "path", "url": "'$1'"}' --file composer.json
